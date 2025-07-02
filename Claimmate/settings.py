6d0195aef21c9c9d8dcfd830768d60dd0843bfca
Kshitij Chawla <ksch002@ess.ais.ac.nz>
2025-07-02 15:22:07 +1200

    Fix CSRF_TRUSTED_ORIGINS and ALLOWED_HOSTS

--- Claimmate/settings.py
+++ Claimmate/settings.py
@@ -27,10 +27,12 @@ SECRET_KEY = 'django-insecure-a1rdp*@i@+rgnzv9kz@61txv*r-qlj!t5p0lvw)z7#i5@cxxrt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [ 'claimmate-app-yourinitials-fuatgcg7hye3c9bt.australiaeast-01.azurewebsites.net',
+    'localhost',
+    '127.0.0.1']
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://claimmate-app-yourinitials-fuatgcg7hye3c9bt.australiaeast-01.azurewebsites.net'
+ 'https://claimmate-app-yourinitials-fuatgcg7hye3c9bt.australiaeast-01.azurewebsites.net'
 ]
 
 
